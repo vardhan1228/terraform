9ec9e0902c3f154c948f58e64d0c132d8fec567f
srivardhan <159407504+vardhan1228@users.noreply.github.com>
2024-03-02 13:16:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
+provider "aws" {
+  
+}
 resource "aws_instance" "vsv" {
-    ami = var.ami
-  key_name = var.key_name
-  instance_type = var.instance_type
-  tags = {
-    Name = "terraform"
-  }
-
+    ami = "ami-0440d3b780d96b29d"
+    instance_type = "t2.micro"
+    key_name = "sri"
+    
+  
 }
