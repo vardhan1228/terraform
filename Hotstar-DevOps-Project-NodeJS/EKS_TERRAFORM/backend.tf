c7d54b0980ecd916a3cf81e9cd3b797aaa8849aa
srivardhan <159407504+vardhan1228@users.noreply.github.com>
2024-04-18 16:18:05 +0530

    Delete Hotstar-DevOps-Project-NodeJS directory

--- Hotstar-DevOps-Project-NodeJS/EKS_TERRAFORM/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "hgfhjhgcfgkbvbnm" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
-# hai to all
